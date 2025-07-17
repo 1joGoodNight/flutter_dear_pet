70b8b0905be1a2837bcdf9179368d8e6c6336fba
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-07-17 19:50:45 +0900

    feat: 상품 모델 추가

---
+++ lib/models/products.dart
@@ -0,0 +1,8 @@
+class Products {
+  String name; //상품이름
+  int price; //상품가격
+  String discriptions; //상품설명
+
+  Products(
+      {required this.name, required this.price, required this.discriptions});
+}
