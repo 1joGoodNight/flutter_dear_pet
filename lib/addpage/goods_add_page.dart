a966b40837ea870eaf1251c8533c27c33bf58c8d
seoyeon-haru <sy5792@naver.com>
2025-07-18 16:34:26 +0900

    Add Container

--- lib/addpage/goods_add_page.dart
+++ lib/addpage/goods_add_page.dart
@@ -8,6 +8,7 @@ class GoodsAddPage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text('DearPet'),
+        centerTitle: false,
       ),
       body: Column(
         children: [
