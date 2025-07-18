8159dd9c54aa80d89cf61be042bd88942be47388
seoyeon-haru <sy5792@naver.com>
2025-07-18 16:34:26 +0900

    Add Container

--- lib/addpage/widget/goods_image.dart
+++ lib/addpage/widget/goods_image.dart
@@ -5,7 +5,24 @@ class GoodsImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
         child: Container(
+          width: double.infinity,
       color: Colors.grey[300]!,
+      child: GestureDetector(
+        onTap: () {},
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset('assets/images/img_noimg.png'),
+            SizedBox(height: 16,),
+            Container(
+              height: 40,
+              width: 144,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
+              color: Colors.white),
+            ),
+            ],
+        ),
+      ),
     ));
   }
 }
