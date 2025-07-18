840082a1a33a129f7993c1d72138e02dbd3c7bd5
seoyeon-haru <sy5792@naver.com>
2025-07-18 12:36:02 +0900

    Add Goods File

---
+++ lib/addpage/widget/goods_bottom.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GoodsBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      color: Colors.yellow,
+    ));
+  }
+}
