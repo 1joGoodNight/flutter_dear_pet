9b7420a55a493b79d824728b05ef7f65ade00bbb
seoyeon-haru <sy5792@naver.com>
2025-07-18 17:43:20 +0900

    Feat Text and Image

--- lib/addpage/goods_add_page.dart
+++ lib/addpage/goods_add_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dear_pet/addpage/widget/goods_bottom.dart';
 import 'package:flutter_dear_pet/addpage/widget/goods_image.dart';
+import 'package:flutter_dear_pet/app_bar.dart';
 
 class GoodsAddPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('DearPet'),
-        centerTitle: false,
-      ),
+      appBar: appBar(),
       body: Column(
         children: [
           GoodsImage(),
